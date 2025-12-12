5a91dd27ebaff39b2398cb017b3d0114ff88ae9f
박철민 <@192.168.219.100>
2025-12-12 22:34:48 +0900

    초기버전 V1

---
+++ GitTest/src/ABC/A.java
@@ -0,0 +1,35 @@
+package ABC;
+
+public class A {
+	public static int larger(int a, int b) {
+		if (a > b) // 노드1
+			return a; // 노드2
+		else
+			return b; // 노드3
+	} // 노드4
+
+	public static int abs(int a) {
+		if (a >= 0)
+			return a;
+		else
+			return -a;
+	}
+
+	public static int median(int a, int b, int c) {
+		if (a <= b) { // 1
+			if (b <= c) // 2
+				return b; // 3
+			else if (a <= c) // 4
+				return c; // 5
+			else
+				return a; // 6
+		} else {
+			if (a <= c) // 7
+				return a; // 8
+			else if (b <= c) // 9
+				return c; // 10
+			else
+				return b; // 11
+		}
+	}
+}
---
+++ GitTest/src/ABC/C.java
@@ -0,0 +1,21 @@
+package ABC;
+
+public class C {
+
+	static int binarySearch(int list[], int key) {
+		int low, high, middle; // 1
+		low = 0;
+		high = list.length - 1;
+
+		while (low <= high) { // 2
+			middle = (low + high) / 2; // 3
+			if (key == list[middle]) // 4
+				return middle; // 5
+			else if (key > list[middle]) // 6
+				low = middle + 1; // 7
+			else
+				high = middle - 1; // 8
+		}
+		return -1; // 9
+	} // 10
+}
